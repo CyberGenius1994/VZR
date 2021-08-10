ce11ff5cd577e3755db37c52d28a640c98fe917f
Ivan Baidiuk <ivan.baidiuk@ning.com>
2021-08-10 22:40:53 +0300

    new block

--- js/index.js
+++ js/index.js
@@ -14,4 +14,15 @@ const swiper = new Swiper('.polices__items', {
   },
 });
 
+const swiper2 = new Swiper('.polices__items', {
+  slidesPerView: 4,
+  spaceBetween: 22,
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.polices__button-next',
+    prevEl: '.police__button-prev',
+  },
+});
+
 
