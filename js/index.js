381de39168815181b2f0e292d2035e73e34ced06
Ivan Baidiuk <ivan.baidiuk@ning.com>
2021-08-28 13:27:58 +0300

    fix

--- js/index.js
+++ js/index.js
@@ -54,10 +54,32 @@ const swiper3 = new Swiper('.swiper-container', {
   },
 });
 
+const modalsBlock = function () {
+  const modals = document.querySelectorAll('[data-modal]');
+
+  modals.forEach(function(trigger) {
+    trigger.addEventListener('click', function(event) {
+      event.preventDefault();
+      const modal = document.getElementById(trigger.dataset.modal);
+      modal.classList.add('open');
+      const exits = modal.querySelectorAll('.modal-exit');
+      exits.forEach(function(exit) {
+        exit.addEventListener('click', function(event) {
+          event.preventDefault();
+          modal.classList.remove('open');
+        });
+        document.onkeydown = function(e) {
+          if (e.key === 'Escape') {
+            modal.classList.remove('open');
+          }
+        };
+      });
+    });
+  });
+}
+
 window.addEventListener("load", function() {
-  swiper.init();
-  swiper2.init();
-  swiper3.init();
+  modalsBlock();
 });
 
 
